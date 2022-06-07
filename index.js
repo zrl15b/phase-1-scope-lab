d1b05f608512816b40b4231393e26ba78cf83933
Zachary <zacharylampi96@gmail.com>
2022-06-07 08:58:01 -0400

    commit tree bff05bcea1312f06b667caee31870a51339e81f1

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+function upperCaseCustomerName() {
+   customerName = "BOB"
+ }
+
+function setBestCustomer() {
+    return bestCustomer = "not bob"
+}
+function overwriteBestCustomer() {
+   bestCustomer = "maybe bob"
+}
+
+const leastFavoriteCustomer= "me"
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "you"
+}
